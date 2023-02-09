6051cb794746d745f4b4059b6523f3c22249e5c5
Chetan Jain <jain.cj.chetan@gmail.com>
2023-02-09 13:48:21 +0530

    sasasa

---
+++ data-structures/trees/BST-implemention.ts
@@ -0,0 +1,80 @@
+class Node {
+	right: Node | null;
+	left: Node | null;
+	value: number;
+	constructor(value: number) {
+		this.value = value;
+		this.right = null;
+		this.left = null;
+	}
+}
+
+class BinarySearchTree {
+	root: null | Node;
+	constructor() {
+		this.root = null;
+	}
+	insert(value: number): Node | null {
+		const newNode = new Node(value);
+		if (!this.root || isNaN(value)) {
+			this.root = newNode;
+			return this.root;
+		}
+		let currentNode = this.root;
+		// we have to traverse the tree until we find the root node
+		while (true) {
+			if (currentNode.value === value) return null;
+			if (value < currentNode.value) {
+				// left
+				if (!currentNode.left) {
+					currentNode.left = newNode;
+					return newNode;
+				}
+				currentNode = currentNode.left;
+			}
+			if (value > currentNode.value) {
+				if (!currentNode.right) {
+					currentNode.right = newNode;
+					return newNode;
+				}
+				currentNode = currentNode.right;
+			}
+		}
+	}
+	lookup(value: number): Node | null {
+		if (!this.root || isNaN(value)) return null;
+		let currentNode = this.root;
+		while (true) {
+			if (currentNode.value === value) return currentNode;
+			if (value < currentNode.value) {
+				if (!currentNode.left) return null;
+				currentNode = currentNode.left;
+			}
+			if (value > currentNode.value) {
+				if (!currentNode.right) return null;
+				currentNode = currentNode.right;
+			}
+		}
+	}
+	remove(value: number): Node | null {}
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+console.log(JSON.stringify(traverse(tree.root)));
+console.log(tree.lookup(15));
+console.log(tree.lookup(151));
+
+function traverse(node: Node | null): Node | null {
+	if (!node) return null;
+	const tree: Node = { value: node.value, left: null, right: null };
+	tree.left = node?.left === null ? null : traverse(node.left);
+	tree.right = node?.right === null ? null : traverse(node.right);
+	return tree;
+}
--- test.ts
+++ test.ts
@@ -1,33 +1,35 @@
-function changeStuff(a: number, b, c) {
-	a = a * 10;
-	b.item = "changed";
-	c = { item: "changed" };
-}
-
-let num = 10;
-let obj1 = { item: "unchanged" };
-let obj2 = { item: "unchanged" };
-
-changeStuff(num, obj1, obj2);
-
-// console.log(num); //10
-// console.log(obj1.item); //changed
-// console.log(obj2.item); //unchanged
-
-const arr = [2, 5, 6, 7, 45, 6, 7, 8];
-function rotateArray(arr: number[], rotate: number) {
-	let initial = arr[rotate < 8 ? rotate : arr.length - rotate];
+const arr: number[] = [2, 5, 6, 7, 45, 6, 7, 8];
+function rotateArray(arr: number[], rotate: number): number[] {
+	const temp = [...arr];
 	for (let i = 0; i < arr.length; i++) {
-		const rotationIndex = i + rotate < 8 ? i + rotate : arr.length - rotate - i;
-		console.log(i, rotationIndex);
-		const temp = arr[rotationIndex];
-		arr[rotationIndex] = arr[i];
-		arr[i] = initial;
-		initial = temp;
+		const rotationIndex = (i + rotate) % arr.length;
+		temp[rotationIndex] = arr[i];
 	}
-	return arr;
+	return temp;
 }
-// console.log(rotateArray(arr, 1));
+// rotate by mutating the array
+function rotate(nums: number[], k: number): void {
+	let i = 0;
+	let temp = undefined;
+	for (let i = nums.length - 1; (i = 0); i--) {
+		const rotationIndex = (i + k) % nums.length;
+		arr[rotationIndex] = temp ? temp : arr[i];
+		if (!temp) temp = arr[rotationIndex];
+
+		console.log(i, ".i, total:", total, "rotationIndex:", rotationIndex);
+		i = (rotationIndex + 1) % nums.length;
+		console.log("nextI: ", i);
+	}
+	arr[arr.length - 1] = temp;
+}
+// console.log(arr.join(" "), 7);
+// console.log(rotateArray(arr, 7).join(" "));
+console.log(arr.join(" "), 1);
+rotate(arr, 1);
+console.log(arr.join(" "));
+// 2, 5, 6, 7, 45, 6, 7, 8
+// 2,2,6,7,45,6,7,8,temp=5
+// 2,6,7,45,6,7,8,5,temp=5
 
 // 345678345
 // 468435735
@@ -43,15 +45,15 @@ function splitEvenOdd(num: number): number {
 		const temp = num % 10;
 		num = Math.floor(num / 10);
 		if (temp % 2 === 0) {
-			console.log("temp", temp, "even", even);
+			// console.log("temp", temp, "even", even);
 			if (even === 0) even = temp;
 			else even = even * 10 + temp;
-			console.log("even", even);
+			// console.log("even", even);
 		} else {
 			odd = odd * 10 + temp;
 		}
 	}
-	console.log(even, odd);
+	// console.log(even, odd);
 }
 
-console.log(splitEvenOdd(345678345));
+// console.log(splitEvenOdd(345678345));
