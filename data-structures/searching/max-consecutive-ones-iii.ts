b018eb50ba5f31a4241257980adcd2cd0c73c2f9
Chetan Jain <jain.cj.chetan@gmail.com>
2023-03-20 21:03:39 +0530

    dfsdf

---
+++ data-structures/searching/max-consecutive-ones-iii.ts
@@ -0,0 +1,29 @@
+// Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+// Example 1:
+// Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+// Output: 6
+// Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+// Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+// Example 2:
+// Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+// Output: 10
+// Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+// Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+function longestOnes(nums: number[], k: number): number {
+	if (nums.length === 0) return 0;
+	if (k === nums.length) return nums.length;
+	let left = 0;
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] === 0) k--;
+		if (k < 0) {
+			if (nums[left] === 0) k++;
+			left++;
+		}
+	}
+	return nums.length - left;
+}
+console.log(longestOnes([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2)); //6
+console.log(longestOnes([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2)); //6
+console.log(longestOnes([0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], 3)); //10
