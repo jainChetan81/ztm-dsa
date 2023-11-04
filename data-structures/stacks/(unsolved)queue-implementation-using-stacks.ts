ed86407614dcca16799c44e926540977e07a9a63
Chetan Jain <jain.cj.chetan@gmail.com>
2023-11-04 23:28:54 +0530

    Refactor queue implementation using stacks

--- data-structures/stacks/(unsolved)queue-implementation-using-stacks.ts
+++ data-structures/stacks/(unsolved)queue-implementation-using-stacks.ts
@@ -15,17 +15,3 @@ class MyQueue {
 	empty(): boolean {}
 }
 
-/**
- * Your MyQueue object will be instantiated and called as such:
- * var obj = new MyQueue()
- * obj.push(x)
- * var param_2 = obj.pop()
- * var param_3 = obj.peek()
- * var param_4 = obj.empty()
- */
-
-const first = useRef(function(){
-  return a+b
-})
-
-
---
+++ data-structures/stacks/minimum-string-length-after-removing-substrings.ts
@@ -0,0 +1,36 @@
+// You are given a string s consisting only of uppercase English letters.
+// You can apply some operations to this string where, in one operation, you can remove any occurrence of one of the substrings "AB" or "CD" from s.
+// Return the minimum possible length of the resulting string that you can obtain.
+// Note that the string concatenates after removing the substring and could produce new "AB" or "CD" substrings.
+
+// Example 1:
+// Input: s = "ABFCACDB"
+// Output: 2
+// Explanation: We can do the following operations:
+// - Remove the substring "ABFCACDB", so s = "FCACDB".
+// - Remove the substring "FCACDB", so s = "FCAB".
+// - Remove the substring "FCAB", so s = "FC".
+// So the resulting length of the string is 2.
+// It can be shown that it is the minimum length that we can obtain.
+
+// Example 2:
+// Input: s = "ACBBD"
+// Output: 5
+// Explanation: We cannot do any operations on the string so the length remains the same.
+function minLength(s: string): number {
+	const stack: string[] = [];
+	for (let i = 0; i < s.length; i++) {
+		// console.log(i, stack);
+		if ((s[i] === "B" && stack[stack.length - 1] === "A") || (s[i] === "D" && stack[stack.length - 1] === "C")) {
+			stack.pop();
+			continue;
+		}
+		stack.push(s[i]);
+	}
+	return stack.length;
+}
+// Example 1
+console.log(minLength("ABFCACDB")); // 2
+// Example 2
+console.log(minLength("ACBBD")); // 5
+console.log(minLength("CCCCDDDD")); // 0
