eb1fb3f4b91abddd2cc90bd82098a4f469d21c35
Chetan Jain <jain.cj.chetan@gmail.com>
2023-11-07 18:59:54 +0530

    dasdassad

---
+++ data-structures/stacks/score-of-parentheses.ts
@@ -0,0 +1,33 @@
+// Given a balanced parentheses string s, return the score of the string.
+// The score of a balanced parentheses string is based on the following rule:
+//     "()" has score 1.
+//     AB has score A + B, where A and B are balanced parentheses strings.
+//     (A) has score 2 * A, where A is a balanced parentheses string.
+
+// Example 1:
+// Input: s = "()"
+// Output: 1
+
+// Example 2:
+// Input: s = "(())"
+// Output: 2
+
+// Example 3:
+// Input: s = "()()"
+// Output: 2
+function scoreOfParentheses(s: string): number {
+	let score = 0;
+	const stack = [];
+	for (let i = 0; i < s.length; i++) {
+		if (s[i] === "(") {
+			stack.push(score);
+			score = 0;
+		} else {
+			score = stack.pop()! + Math.max(score * 2, 1);
+		}
+	}
+	return score;
+}
+console.log(scoreOfParentheses("()")); // 1
+console.log(scoreOfParentheses("(())")); // 2
+console.log(scoreOfParentheses("()()")); // 2
